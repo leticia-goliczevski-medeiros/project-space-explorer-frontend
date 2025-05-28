e0ea6797ccdd1bdc682497db2af1b662ec826620
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-05-28 18:30:36 -0300

    feat: create footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__credits">&#169; Author: Letícia Goliczevski Medeiros</p>
+    </footer>
+  )
+}
+
+export default Footer;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
-import Header from '../components/Header.jsx';
-import Footer from '../components/Footer.jsx';
+import Header from '../components/Header/Header.jsx';
+import Footer from '../components/Footer/Footer.jsx';
 
 function MainLayout({ children }){
   return (
