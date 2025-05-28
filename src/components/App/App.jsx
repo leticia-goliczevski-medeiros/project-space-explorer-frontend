342e360e3d586fa73b1e8057a357e9998bbb0343
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-05-28 17:42:19 -0300

    feat: add App.css and set up initial routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <div className="page">
+        <Routes>
+          <Route path="/" element={
+            
+          } />
+          <Route path="/signin" element={
+            
+          } />
+          <Route path="/signup" element={
+            
+          } />
+          <Route path="/mygallery" element={
+            
+          } />
+          <Route path="/explore" element={
+            
+          } />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
