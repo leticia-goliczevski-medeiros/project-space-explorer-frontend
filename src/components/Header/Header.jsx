51effdde548e3254bdb9c9c40c361d29d429ce78
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-06-03 14:48:35 -0300

    styel: style header and footer components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import './Header.css';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import menuIcon from '../../images/menu-icon.png';
+import closeIcon from '../../images/close-icon.png';
 
 import UserNavigation from './UserNavigation/UserNavigation.jsx';
 import AuthNavigation from './AuthNavigation/AuthNavigation.jsx';
@@ -9,14 +12,31 @@ import IsLoggedInContext from '../../contexts/IsLoggedInContext';
 
 function Header() {
  const { isLoggedIn } = useContext(IsLoggedInContext);
+ const [ isMenuOpen, setIsMenuOpen ] = useState(false);
 
   return (
     <header className='header'>
-      <Link to="/" className='header__title'><h1>SPACE EXPLORER</h1></Link>
+      <div className="header__navbar">
+        <Link to="/" className='header__title'><h1>SPACE EXPLORER</h1></Link>
 
-      <nav className='header__nav'>
-        {isLoggedIn? <UserNavigation /> : <AuthNavigation />}
-      </nav>
+        <nav className='header__links header__links_desktop'>
+          {isLoggedIn? <UserNavigation /> : <AuthNavigation />}
+        </nav>
+
+        <button className='header__menu-button' onClick={()=> setIsMenuOpen(!isMenuOpen)}>
+          {isMenuOpen? 
+            <img src={closeIcon} alt="Close icon." className="header__menu-icon" /> 
+            : 
+            <img src={menuIcon} alt="Menu icon." className="header__menu-icon" />
+          }
+        </button>
+      </div>
+
+      {isMenuOpen && (
+        <nav className='header__links header__links_mobile'>
+          {isLoggedIn? <UserNavigation /> : <AuthNavigation />}
+        </nav>
+      )}
     </header>
   )
 }
